ecff12ca521c8e6d270b607de826c7bd9ba6a38b
cealvarez93 <ceal1993@gmail.com>
2025-06-27 00:47:19 -0400

    added content

--- index.cjs
+++ index.cjs
@@ -144,6 +144,7 @@ const SOUNDS = {
   oh_yeah:                'https://raw.githubusercontent.com/cealvarez93/mi-soundboard/main/sounds/oh_yeah.mp4',
   escucha_las_palabras:                'https://raw.githubusercontent.com/cealvarez93/mi-soundboard/main/sounds/escucha_las_palabras.mp4',
   hana_ni_natte:                'https://raw.githubusercontent.com/cealvarez93/mi-soundboard/main/sounds/hana_ni_natte.mp4',
+  she_was_a_fairy:                'https://raw.githubusercontent.com/cealvarez93/mi-soundboard/main/sounds/she_was_a_fairy.mp4',
 };
 
 // Guardamos el guildId donde está la conexión activa
