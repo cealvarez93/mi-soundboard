2d081377b9fac241c9e7a4001f8db40fa447e544
cealvarez93 <ceal1993@gmail.com>
2025-06-29 22:04:08 -0400

    added content

--- index.cjs
+++ index.cjs
@@ -154,6 +154,8 @@ const SOUNDS = {
   penelope:                'https://raw.githubusercontent.com/cealvarez93/mi-soundboard/main/sounds/penelope.mp4',
   la_polizia_no:                'https://raw.githubusercontent.com/cealvarez93/mi-soundboard/main/sounds/la_polizia_no.mp4',
   me_mataron:                'https://raw.githubusercontent.com/cealvarez93/mi-soundboard/main/sounds/me_mataron.mp4',
+  la_gata:                'https://raw.githubusercontent.com/cealvarez93/mi-soundboard/main/sounds/la_gata.mp4',
+  bailo:                'https://raw.githubusercontent.com/cealvarez93/mi-soundboard/main/sounds/bailo.mp4',
 };
 
 // Guardamos el guildId donde está la conexión activa
